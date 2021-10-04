b25704c9df4ba3fc7f354b0c027c7c418684c109
Daniel Webiörn <spaxys@live.se>
2021-10-04 11:39:18 +0200

    #4

---
+++ Scripts/fileReader.js
@@ -0,0 +1,15 @@
+function FileHelper()
+
+{
+    FileHelper.readStringFromFileAtPath = function(pathOfFileToReadFrom)
+    {
+        var request = new XMLHttpRequest();
+        request.open("GET", pathOfFileToReadFrom, false);
+        request.send(null);
+        var returnValue = request.responseText;
+
+        return returnValue;
+    }
+}
+
+
